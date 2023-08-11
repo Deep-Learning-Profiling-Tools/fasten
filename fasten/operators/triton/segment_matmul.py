402daf0107cb37396d0b6bfebb2609cba4c3f12d
Karthikg99 <gkarthik754@gmail.com>
2023-08-11 12:13:51 -0400

    modified segemnt matmul

--- fasten/operators/triton/segment_matmul.py
+++ fasten/operators/triton/segment_matmul.py
@@ -1,7 +1,18 @@
 import torch
 import triton
 import triton.language as tl
+import os
+import numpy as np
+import bisect
 import pyg_lib
+
+@triton.autotune(
+    configs=[
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32, 'GROUP_SIZE_M': 8}, num_stages=1, num_warps=8),
+    ],
+    key=['M', 'N', 'K'],
+)
+
 
 
 @triton.jit
@@ -9,12 +20,14 @@ def segment_matmul_kernel(
     inputs_ptr, others_ptr, c_ptr,
     M, N, K,
     stride_input_m, stride_input_k,
-    stride_other_b, stride_other_k, stride_other_n,
+    stride_other_z, stride_other_k,stride_other_n,
     stride_output_m, stride_output_n,
     start_bounds, end_bounds, batch_idx, len_m,
+    # Meta-parameters
     BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
-    GROUP_SIZE_M: tl.constexpr = 8,
+    GROUP_SIZE_M: tl.constexpr= 8,
 ):
+
     pid = tl.program_id(axis=0)
     num_pid_m = len_m
     num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
@@ -25,27 +38,30 @@ def segment_matmul_kernel(
     pid_m = first_pid_m + (pid % group_size_m)
     pid_n = (pid % num_pid_in_group) // group_size_m
 
-    start_offs_input_m = tl.load(start_bounds + pid_m)
-    end_offs_input_m = tl.load(end_bounds + pid_m)
-
+   
+    start_offs_input_m= tl.load(start_bounds+ pid_m)
+    end_offs_input_m= tl.load(end_bounds+ pid_m)
+    batch_idx_other= tl.load(batch_idx + pid_m)
     if start_offs_input_m >= end_offs_input_m:
-        return
+      return
 
-    batch_idx_other = tl.load(batch_idx + pid_m)
-
-    offs_input_m = start_offs_input_m + tl.arange(0, BLOCK_SIZE_M)
+    offs_input_m=(start_offs_input_m + tl.arange(0, BLOCK_SIZE_M)) 
     offs_other_n = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
     offs_k = tl.arange(0, BLOCK_SIZE_K)
 
+
     input_ptrs = inputs_ptr + \
-        (offs_input_m[:, None] * stride_input_m + offs_k[None, :] * stride_input_k)
-    other_ptrs = others_ptr + batch_idx_other * stride_other_b + \
-        (offs_k[:, None] * stride_other_k + offs_other_n[None, :] * stride_other_n)
+         (offs_input_m[:, None] * stride_input_m + offs_k[None, :] * stride_input_k)
+    other_ptrs = others_ptr + batch_idx_other * stride_other_z + \
+         (offs_k[:, None] * stride_other_k + offs_other_n[None, :] * stride_other_n)
+ 
+
 
     accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
-    input_mask = offs_input_m[:, None] < end_offs_input_m
+    input_mask=offs_input_m[:, None]<end_offs_input_m
 
     for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+
         input_k_mask = (offs_k[None, :] < K - k * BLOCK_SIZE_K)
         a = tl.load(input_ptrs, mask=input_mask & input_k_mask, other=0.0)
         b = tl.load(other_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
@@ -55,82 +71,82 @@ def segment_matmul_kernel(
 
     c = accumulator.to(tl.float32)
 
-    offs_cm = start_offs_input_m + tl.arange(0, BLOCK_SIZE_M)
-    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-    c_ptrs = c_ptr + stride_output_m * \
-        offs_cm[:, None] + stride_output_n * offs_cn[None, :]
-    c_mask = (offs_cm[:, None] < end_offs_input_m) & (offs_cn[None, :] < N)
+    offs_cm= (start_offs_input_m + tl.arange(0, BLOCK_SIZE_M)) 
+    offs_cn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) 
+    c_ptrs = c_ptr + stride_output_m * offs_cm[:, None] + stride_output_n * offs_cn[None, :]
+    c_mask = (offs_cm[:,None]<end_offs_input_m ) & (offs_cn[None, :] < N)
     tl.store(c_ptrs, c, mask=c_mask)
 
-# TODO(Keren): this function is here just for testing purpose,
-# we should have start_bounds and end_bounds on GPU when initializing the model
 
 
-def triton_segment_matmul(inputs, ptr, others, BLOCK_M=64, BLOCK_N=64, BLOCK_K=32):
-    M, K = inputs.shape
-    _, K, N = others.shape
 
-    start_bounds = []
-    for i in range(len(ptr)-1):
-        if ptr[i] == ptr[i+1]:
-            for j in range(ptr[i], ptr[i+1]+1, BLOCK_M):
-                start_bounds.append(j)
-        else:
-            for j in range(ptr[i], ptr[i+1], BLOCK_M):
-                start_bounds.append(j)
 
-    end_bounds = start_bounds[1:]
-    end_bounds.append(ptr[-1])
-    end_bounds = torch.tensor(end_bounds, dtype=torch.int32, device=inputs.device)
+def triton_segment_matmul(inputs, ptr, other, BLOCK_M=64, BLOCK_N=64, BLOCK_K=32):
+  M, K = inputs.shape
+  P, K, N = other.shape
+  start_bounds=[]
+  for i in range(len(ptr)-1):
+    if ptr[i]== ptr[i+1]:
+      for j in range(ptr[i], ptr[i+1]+1, BLOCK_M):
+        start_bounds.append(j)
+    else:
+      for j in range(ptr[i], ptr[i+1], BLOCK_M):
+        start_bounds.append(j)
 
-    num_m_blocks = len(start_bounds)
-    batch_idx = []
-    ptr_idx = 1
-    b = 0
-    for j in range(0, num_m_blocks):
-        if start_bounds[j]//ptr[ptr_idx] == 0:
-            batch_idx.append(b)
-        else:
-            b += 1
-            batch_idx.append(b)
-            ptr_idx += 1
+  end_bounds= start_bounds[1:]
+  end_bounds.append(ptr[-1])
+  end_bounds=torch.tensor(end_bounds, dtype=torch.int32)
+  
+  len_m= len(start_bounds)
+  batch_idx=[]
+  len_ptr=1
+  matrix_count=0
+  for j in range(0,len_m):
+      if start_bounds[j]//ptr[len_ptr]==0:
+        batch_idx.append(matrix_count)
+      else:
+        matrix_count+=1
+        batch_idx.append(matrix_count)
+        len_ptr+=1
+  
+  start_bounds=torch.tensor(start_bounds, dtype=torch.int32)
+  assert len(start_bounds)==len(batch_idx)
+  batch_idx=torch.tensor(batch_idx, dtype=torch.int32)
 
-    start_bounds = torch.tensor(start_bounds, dtype=torch.int32, device=inputs.device)
-    batch_idx = torch.tensor(batch_idx, dtype=torch.int32, device=inputs.device)
 
-    output = torch.empty((M, N), device=inputs.device, dtype=inputs.dtype)
-    # 1D launch kernel where each block gets its own program.
-    grid = (num_m_blocks * triton.cdiv(N, BLOCK_N),)
-    segment_matmul_kernel[grid](
-        inputs, others, output,
+  start_bounds=start_bounds.cuda()
+  end_bounds= end_bounds.cuda()
+  batch_idx= batch_idx.cuda()
+
+
+
+
+  output = torch.empty((M, N), device=inputs.device, dtype=inputs.dtype, requires_grad= True)
+   
+  grid = lambda META: (
+         len_m * triton.cdiv(N, BLOCK_N),
+  )
+  start_event = torch.cuda.Event(enable_timing = True)
+  end_event = torch.cuda.Event(enable_timing = True)
+  start_event.record()
+  segment_matmul_kernel[grid](
+        inputs, other, output,
         M, N, K,
         inputs.stride(0), inputs.stride(1),
-        others.stride(0), others.stride(1), others.stride(2),
+        other.stride(0), other.stride(1), other.stride(2),
         output.stride(0), output.stride(1),
-        start_bounds, end_bounds, batch_idx, num_m_blocks,
-        BLOCK_SIZE_M=BLOCK_M, BLOCK_SIZE_N=BLOCK_N, BLOCK_SIZE_K=BLOCK_K,
-    )
-    return output
-
-
-def torch_segment_matmul(inputs, ptrs, others):
-    M, K = inputs.shape
-    _, K, N = others.shape
-    output = torch.empty((M, N), device=inputs.device, dtype=inputs.dtype)
-
-    for i in range(len(ptrs) - 1):
-        if ptrs[i] == ptrs[i+1]:
-            continue
-        else:
-            output[ptrs[i]:ptrs[i+1],
-                   :] = torch.matmul(inputs[ptrs[i]:ptrs[i+1], :], others[i])
-    return output
+        start_bounds, end_bounds, batch_idx, len_m,
+  )
+  end_event.record()
+  torch.cuda.synchronize()
+  print("Triton time:",start_event.elapsed_time(end_event))
+  return output
 
 
 torch.manual_seed(42)
-inputs = torch.randn(128*200, 16).cuda()
-ptrs = torch.tensor([0, 256, 505, 505, 729, 128*10, 128*20, 128*200, 128*200]).cuda()
-other = torch.randn(8, 16, 16).cuda()
-triton_output = triton_segment_matmul(inputs, ptrs, other)
-pyg_output = pyg_lib.ops.segment_matmul(inputs, ptrs, other)
-print(torch.max(torch.abs(triton_output-pyg_output)))
+inputs = torch.randn(1029, 16).cuda()
+ptr=torch.tensor([0,257,270,377,377,1029,1029, 1029]).cuda()
+other = torch.randn(7, 16, 4).cuda()
+triton_output= triton_segment_matmul(inputs, ptr, other)
+torch_output = pyg_lib.ops.segment_matmul(inputs, ptr, other)
+print(torch.max(torch.abs(triton_output-torch_output)))
