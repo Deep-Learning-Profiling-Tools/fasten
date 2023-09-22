a7df1542cfcd7ae54a2bd3d1e17192ea7ce60a44
Karthikg99 <gkarthik754@gmail.com>
2023-09-22 14:01:49 -0400

    modified:   test/test_nn.py

--- test/test_nn.py
+++ test/test_nn.py
@@ -35,31 +35,29 @@ def test_correctness():
     rgcn_conv = RGCNConv(4, 32, 8).to(device)
     rgcn_conv_out = rgcn_conv(x, edge_index, edge_type)
 
-    
     tensor_slice, edge_index, edge_type = tensor_slice_gen(edge_type, edge_index, num_types)
     torch.manual_seed(12345)
     fasten_rgcn_conv = FastenRGCNConv(4, 32, 8).to(device)
     fasten_rgcn_conv_out = fasten_rgcn_conv(x, edge_index, edge_type, tensor_slice)
 
     assert fasten_rgcn_conv_out.shape == rgcn_conv_out.shape
-    torch.testing.assert_close(fasten_rgcn_conv_out, rgcn_conv_out)
+    torch.testing.assert_close(fasten_rgcn_conv_out, rgcn_conv_out, atol = 1e-3, rtol = 1e-2)
 
 
 def test_benchmark():
 
     num_nodes = 10000
-    num_features = 320
-    num_edges = 20000
-    num_types = 32
+    num_features = 32
+    num_edges = 2000
+    num_types = 8
 
     x = torch.randn(num_nodes, num_features, device=device)
     edge_index = torch.randint(0, num_nodes, (2, num_edges), device=device)
     edge_type = torch.randint(0, num_types, (num_edges,), device=device)
 
-
     tensor_slice, edge_index, edge_type = tensor_slice_gen(edge_type, edge_index, num_types)
     torch.manual_seed(12345)
-    rgcn_conv = RGCNConv(num_features, num_features, num_types, is_sorted = True).to(device)
+    rgcn_conv = RGCNConv(num_features, num_features, num_types, is_sorted=True).to(device)
     ms = triton.testing.do_bench(lambda: rgcn_conv(x, edge_index, edge_type))
     print("Torch time:", ms)
 
