3595ff53dde0e0f150d921c2879aa34944b98579
123standup <plin8@ncsu.edu>
2023-10-21 22:01:27 -0400

    update

--- test/test_ops.py
+++ test/test_ops.py
@@ -1,31 +1,22 @@
 import pyg_lib
 import pytest
 import torch
-<<<<<<< HEAD
 import triton
-=======
-import pyg_lib
-import triton
-
->>>>>>> 6228019 (add test_bench)
+import json
+import csv
+from typing import Callable
 from utils import read_slices_from_csv
 
 from fasten import Engine, compact_tensor_types, ops
 
 slices0 = [slice(0, 63), slice(63, 90), slice(90, 128)]
 slices1 = [slice(0, 127), slice(127, 256), slice(256, 257), slice(257, 512)]
-<<<<<<< HEAD
-=======
-# ptr = torch.tensor([0,63,90,128])
->>>>>>> 6228019 (add test_bench)
 AIFB = read_slices_from_csv('AIFB.csv')
 AM = read_slices_from_csv('AM.csv')
 BGS = read_slices_from_csv('BGS.csv')
 DBLP = read_slices_from_csv('DBLP.csv')
 MUTAG = read_slices_from_csv('MUTAG.csv')
-
 slices_obj = [("AIFB", AIFB), ("AM", AM), ("BGS", BGS), ("DBLP", DBLP), ("MUTAG", MUTAG)]
-
 
 @pytest.mark.parametrize("device", ["cpu", "cuda"])
 @pytest.mark.parametrize("engine", [Engine.TORCH, Engine.TRITON])
@@ -75,20 +66,31 @@ def test_segment_matmul(K: int, T: int, slices: list, engine: Engine, device: st
             torch.testing.assert_close(other.grad, other_grad_ref, atol=1.0, rtol=1e-2)
         else:
             torch.testing.assert_close(other.grad, other_grad_ref, atol=1e-1, rtol=1e-2)
-<<<<<<< HEAD
     if device == "cuda":
         torch.cuda.empty_cache()
+
+
+@pytest.fixture(scope="session")
+def benchmark_results():
+    results = []
+    yield results
+
+    with open("benchmark_results.json", "w") as json_file:
+        json.dump(results, json_file, indent=4)
+    
+    header = results[0].keys()
+    with open("benchmark_results.csv", "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=header)
+        writer.writeheader()
+        for row in results:
+            writer.writerow(row)
 
 
 @pytest.mark.parametrize("phase", ["forward", "backward", "full"])
 @pytest.mark.parametrize("dtype", ["float32"])  # pyg_lib doesn't support float16
 @pytest.mark.parametrize("slices_name, slices", slices_obj)
 @pytest.mark.parametrize("K", [16, 32, 64])
-<<<<<<< HEAD
-def test_perf(phase: str, dtype: str, slices: list, K: int) -> None:
-=======
-def test_bench(phase: str, dtype: str, slices: list, K: int) -> None:
->>>>>>> 780f990 (Update)
+def test_perf(phase: str, dtype: str, slices_name: str, slices: list, K: int, benchmark_results: Callable[[], None]) -> None:
     T = len(slices)
     M = sum([s.stop - s.start for s in slices])
     dtype = getattr(torch, dtype)
@@ -132,8 +134,8 @@ def test_bench(phase: str, dtype: str, slices: list, K: int) -> None:
 
     fasten_ms = triton.testing.do_bench(fasten_fn)
     pyg_ms = triton.testing.do_bench(pyg_fn)
-    print(f"phase: {phase} fasten: {fasten_ms} ms vs pyg: {pyg_ms} ms")
-    
+    print(f"{phase}: fasten: {fasten_ms} ms vs pyg: {pyg_ms} ms")
+
     benchmark_results.append({
         "phase": phase,
         "dataset": slices_name,
@@ -158,100 +160,3 @@ def test_cache():
     ops.fasten_segment_matmul(tensor_slice.data, other, tensor_slice, Engine.TRITON)
     assert len(tensor_slice._cache) == 1
     assert len(tensor_slice._cache['segment_matmul_forward']) == 1
-<<<<<<< HEAD
-=======
-
-@pytest.mark.test_bench
-def test_bench(iter=1) -> None:
-    import sys
-    import itertools
-
-    # benchmark factors
-    devices = ['cuda']
-    engine = [Engine.TRITON]
-    phase  = ['forward']
-    dtypes  = ['float32'] 
-    slices = [AIFB]
-    slices_name = ['AIFB']
-    # slices = [AIFB, AM, BGS, DBLP, MUTAG]
-    # slices_name = ['AIFB', 'AM', 'BGS', 'DBLP', 'MUTAG']
-    K = [16, 32, 64, 80]
-    methods = ['fasten', 'pyg']
-    
-    results = []
-    for i, s in enumerate(slices):
-        T = len(s)
-        ptr = torch.tensor([seg.start for seg in s] + [s[-1].stop])
-
-        for k, e, p, d, dt, m in itertools.product(K, engine, phase, devices, dtypes, methods):
-            if e == Engine.TRITON and d == 'cpu':
-                continue
-            if d == 'cpu' and dt == 'float16':
-                continue
-            
-            # prepare data
-            dtype = getattr(torch, dt)
-            M = sum([seg.stop - seg.start for seg in s])
-            data = torch.randn((M, k), device=d, dtype=dtype)
-            types = torch.zeros((M,), device=d, dtype=torch.int)
-            # for seg in s:
-            #     if seg.stop > seg.start:
-            #         types[seg] = torch.randint(0, T, (seg.stop - seg.start,), device=d, dtype=torch.int)
-            sorted_data, tensor_slice = compact_tensor_types(data, types, device=d)
-            other = torch.randn((T, k, k), device=d, dtype=dtype)
-
-            label = f'{m}, {k}, {slices_name[i]}, {p}'
-            
-            # warmp up
-            # for _ in range(5):
-            #     try: 
-            #         if m == 'fasten':
-            #             ops.fasten_segment_matmul(sorted_data, tensor_slice.slices, other, e, tensor_slice)
-            #         elif m == 'pyg':
-            #             output = pyg_lib.ops.segment_matmul(sorted_data, ptr, other)
-            #     except Exception:
-            #         print(label, ': error')
-            #         break
-
-            # start timer
-            # start_event = torch.cuda.Event(enable_timing = True)
-            # end_event = torch.cuda.Event(enable_timing = True)
-            # start_event.record()
-            stream = torch.cuda.Stream()
-            torch.cuda.set_stream(stream)
-
-            for _ in range(iter):
-                try: 
-                    if m == 'fasten':
-                        def fasten_fn():
-                            ops.fasten_segment_matmul(sorted_data, tensor_slice.slices, other, e, tensor_slice)
-                        ms = triton.testing.do_bench(fasten_fn)
-                        # ms = triton.testing.do_bench_cudagraph(fasten_fn)
-                        print('fasten: ', ms)
-
-                    elif m == 'pyg':
-                        def pyg_fn():
-                            pyg_lib.ops.segment_matmul(sorted_data, ptr, other)
-                        ms = triton.testing.do_bench(pyg_fn)
-                        # ms = triton.testing.do_bench_cudagraph(pyg_fn)
-                        print('pyg: ', ms)
-
-                except Exception as error:
-                    print(label, ': error', error)
-                    break
-            # end_event.record()
-            # torch.cuda.synchronize()
-            # results.append(f'{m}, {k}, {slices_name[i]}, {p}, {start_event.elapsed_time(end_event)/iter}')
-            # print(results[-1])
-    
-    f = open('bench.csv', 'w')
-    f.write("Method, K, Data, Phase, Time\n")
-    for r in results:
-        f.write(r)
-        f.write('\n')
-    f.close()
-
-test_bench()
->>>>>>> 6228019 (add test_bench)
-=======
->>>>>>> 9973a1c (Update)
