eb882d06134d61697a8c6f3c6d44b8ae31986b08
Jokeren <robinho364@gmail.com>
2023-10-26 16:08:42 -0400

    Update

--- fasten/scheduler.py
+++ fasten/scheduler.py
@@ -66,9 +66,7 @@ def balance_tiling(slices: list, tile_size: int, large_tile_size: int, subslices
         least_space_left = float('inf')
 
         for i, bin in enumerate(bins):
-            bin_size = len(bin[0])
-            # At maximum large_tile_size
-            if bin_size < large_tile_size and bin[1] >= slice_length and bin[1] - slice_length < least_space_left:
+            if bin[1] >= slice_length and bin[1] - slice_length < least_space_left:
                 least_space_left = bin[1] - slice_length
                 best_fit_bin_idx = i
 
