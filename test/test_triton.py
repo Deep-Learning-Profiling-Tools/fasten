352707b2c377bfe2840945014c56684f2a743689
Jokeren <robinho364@gmail.com>
2023-10-28 15:51:00 -0400

    Update

--- test/test_triton.py
+++ test/test_triton.py
@@ -11,10 +11,11 @@ from fasten.utils import TilingMethod
 @pytest.mark.parametrize("M", [128, 1024])
 @pytest.mark.parametrize("T", [16, 33])
 @pytest.mark.parametrize("tile_size", [16, 64])
+@pytest.mark.parametrize("block_size", [1, 4])
 @pytest.mark.parametrize("K", [16, 80])
 @pytest.mark.parametrize("device", ["cuda"])
 @pytest.mark.parametrize("tiling_method", ["default", "balanced"])
-def test_segment_matmul(M: int, K: int, T: int, phase: str, dtype: str, tile_size: int, device: str, tiling_method: str) -> None:
+def test_segment_matmul(M: int, K: int, T: int, phase: str, dtype: str, tile_size: int, block_size: int, device: str, tiling_method: str) -> None:
     dtype = getattr(torch, dtype)
     data = torch.randn((M, K), dtype=dtype, device=device)
     types = torch.randint(0, T, (M,), device=device, dtype=torch.int)
@@ -22,7 +23,7 @@ def test_segment_matmul(M: int, K: int, T: int, phase: str, dtype: str, tile_siz
     other = torch.randn((T, K, K), device=device, dtype=dtype)
     tiling_method = getattr(TilingMethod, tiling_method.upper())
     if phase == "forward":
-        input_tiles = tensor_slice.tiling(tile_size, method=tiling_method)
+        input_tiles = tensor_slice.tiling(tile_size, method=tiling_method, block_size=block_size)
         output = triton_ops.segment_matmul_forward(tensor_slice.data, other, input_tiles.slices, input_slices=tensor_slice.slices, tile_size=tile_size, out_dtype=torch.float32, num_blocks=input_tiles.num_blocks, block_size=input_tiles.block_size)
         output_ref = torch.zeros((M, K), dtype=dtype, device="cuda")
         for i in range(len(tensor_slice)):
@@ -31,7 +32,7 @@ def test_segment_matmul(M: int, K: int, T: int, phase: str, dtype: str, tile_siz
             output_ref[s] = torch.matmul(tensor_slice.data[s], other[t])
         torch.testing.assert_close(output, output_ref, atol=1e-1, rtol=1e-2)
     elif phase == "backward":
-        input_tiles = tensor_slice.tiling(tile_size, method=tiling_method)
+        input_tiles = tensor_slice.tiling(tile_size, method=tiling_method, block_size=block_size)
         output = triton_ops.segment_matmul_forward(tensor_slice.data, other, input_tiles.slices, input_slices=tensor_slice.slices, tile_size=tile_size, num_blocks=input_tiles.num_blocks, block_size=input_tiles.block_size)
         output_grad = torch.randn_like(output)
         grad_input, grad_other = triton_ops.segment_matmul_backward(tensor_slice.data, output_grad, other, input_tiles.slices, input_slices=tensor_slice.slices, tile_size=tile_size, num_blocks=input_tiles.num_blocks, block_size=input_tiles.block_size)
