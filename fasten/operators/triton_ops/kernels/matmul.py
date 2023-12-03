090a119eff2cdcd4404233880870ff572ff3b136
Jokeren <robinho364@gmail.com>
2023-12-02 20:40:38 -0500

    Update

--- fasten/operators/triton_ops/kernels/matmul.py
+++ fasten/operators/triton_ops/kernels/matmul.py
@@ -136,7 +136,6 @@ def _fused_matmul(
 ):
     offs_m = start_off + tl.arange(0, TILE_M)
     offs_n = pid_n * TILE_N + tl.arange(0, TILE_N)
-    mask_n = (offs_n[None, :] < N)
     offs_k = tl.arange(0, TILE_K)
     rn = tl.max_contiguous(tl.multiple_of(offs_n % N, TILE_N), TILE_N)
 
@@ -146,20 +145,16 @@ def _fused_matmul(
         (offs_k[:, None] * stride_other_k + rn[None, :] * stride_other_n)
 
     acc = tl.zeros((TILE_M, TILE_N), dtype=out_dtype)
+    mask_m = offs_m[:, None] < end_off
 
-    kiters = K // TILE_K if EVEN_K else tl.cdiv(K, TILE_K)
-    iters = kiters * BLOCK_SIZE
+    k_iters = (K // TILE_K if EVEN_K else tl.cdiv(K, TILE_K)) * BLOCK_SIZE
     k = 0
-    for _ in range(0, iters):
-        if EVEN_K:
-            a = tl.load(input_ptrs)
-            b = tl.load(other_ptrs)
-        else:
-            a = tl.load(input_ptrs, mask=(offs_k[None, :] + k * TILE_K < K), other=0.0)
-            b = tl.load(other_ptrs, mask=(offs_k[:, None] + k * TILE_K < K), other=0.0)
+    for _ in range(0, k_iters):
+        a = tl.load(input_ptrs, mask=mask_m & (offs_k[None, :] + k * TILE_K < K), other=0.0)
+        b = tl.load(other_ptrs, mask=(offs_k[:, None] + k * TILE_K < K), other=0.0)
         acc += tl.dot(a, b, out_dtype=out_dtype)
-        c_ptrs = output + stride_output_m * \
-            offs_m[:, None] + stride_output_n * offs_n[None, :]
+        input_ptrs += TILE_K * stride_input_k
+        other_ptrs += TILE_K * stride_other_k
         if k % BLOCK_SIZE == BLOCK_SIZE - 1:
             acc = acc.to(output.dtype.element_ty)
             acc = tl.zeros((TILE_M, TILE_N), dtype=out_dtype)
@@ -167,7 +162,9 @@ def _fused_matmul(
             input_ptrs = input + (offs_m[:, None] * stride_input_m + offs_k[None, :] * stride_input_k)
             other_ptrs = other + \
                 (offs_k[:, None] * stride_other_k + rn[None, :] * stride_other_n)
+            c_ptrs = output + stride_output_m * offs_m[:, None] + stride_output_n * offs_n[None, :]
             mask_m = offs_m[:, None] < end_off
+            mask_n = offs_n[None, :] < N
             tl.store(c_ptrs, acc, mask_n & mask_m)
             k = 0
         else:
