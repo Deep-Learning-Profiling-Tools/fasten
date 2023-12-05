0e8fc50532138fef8a5a49e7e9869880a4c56bd6
Karthik <88997741+Karthikg99@users.noreply.github.com>
2023-12-05 13:04:13 -0500

    FastenHeteroLinear and HeatConv (#48)

--- fasten/nn/__init__.py
+++ fasten/nn/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa: F401
-from .conv import FastenHGTConv, FastenRGCNConv
+from .conv import FastenHEATConv, FastenHGTConv, FastenRGCNConv
 from .linear import FastenHeteroDictLinear
--- fasten/nn/linear/__init__.py
+++ fasten/nn/linear/__init__.py
@@ -1,2 +1,2 @@
 # flake8: noqa: F401
-from .linear import FastenHeteroDictLinear
+from .linear import FastenHeteroDictLinear, FastenHeteroLinear
