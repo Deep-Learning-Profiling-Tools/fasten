adcaec08e89009c0e2e3e8b0c782250a23d1a421
Keren Zhou <robinho364@gmail.com>
2023-12-16 20:06:41 -0500

    Segmented matmul flops calculation (#69)

---
+++ fasten/stats.py
@@ -0,0 +1,14 @@
+import torch
+
+from .tensor_slice import TensorSlice
+
+
+def get_matmul_flops(input: TensorSlice, weight: torch.Tensor):
+    assert weight.dim() == 3, f"weight dim should be 3, got {weight.dim()}"
+    flops = 0
+    for i in range(len(input)):
+        s = input.get_slice_from_index(i, is_tensor=False)
+        if s.stop - s.start == 0:
+            continue
+        flops += (s.stop - s.start) * weight.shape[1] * weight.shape[2] * 2
+    return flops
