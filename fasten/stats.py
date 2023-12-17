99e4988b072be812a16400533ad5722b145eeea7
Jokeren <robinho364@gmail.com>
2023-12-16 19:50:34 -0500

    Update

--- fasten/stats.py
+++ fasten/stats.py
@@ -10,5 +10,5 @@ def get_matmul_flops(input: TensorSlice, weight: torch.Tensor):
         s = input.get_slice_from_index(i, is_tensor=False)
         if s.stop - s.start == 0:
             continue
-        flops += (s.stop - s.start) * weight.shape[1] * weight.shape[2]
+        flops += (s.stop - s.start) * weight.shape[1] * weight.shape[2] * 2
     return flops
--- test/test_stats.py
+++ test/test_stats.py
@@ -8,6 +8,7 @@ def test_matmul_flops():
     data = torch.tensor([[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8]])
     types = torch.tensor([2, 1, 2], dtype=torch.int)
     tensor_slice = compact_tensor_types(data, types)
-    print(tensor_slice.slices)
     weight = torch.randn((3, 4, 5))
-    print(get_matmul_flops(tensor_slice, weight))
+    flops = get_matmul_flops(tensor_slice, weight)
+    flops_ref = 2 * 4 * 5 * 2 + 4 * 5 * 2
+    assert flops == flops_ref, f"{flops} != {flops_ref}"
