d8f404faef8cc987cb684247294fa022ec2bdeab
Jokeren <robinho364@gmail.com>
2024-01-13 12:23:57 -0500

    Update

--- fasten/operators/triton_ops/segment_matmul.py
+++ fasten/operators/triton_ops/segment_matmul.py
@@ -129,23 +129,24 @@ def _noncontiguous_block(
 
             if length > 0:
                 type_id = tl.load(input_tiles + 5 * next_id + 1)
-                _dispatch(
-                    pid_n,
-                    start_off, end_off,
-                    input, other + type_id * stride_other_b, output,
-                    K, N,
-                    stride_input_m, stride_input_k,
-                    stride_other_k, stride_other_n,
-                    stride_output_m, stride_output_n,
-                    out_dtype=out_dtype,
-                    MASK_M=True,
-                    EVEN_K=EVEN_K,
-                    EVEN_N=EVEN_N,
-                    TILE_M=TILE_M,
-                    TILE_N=TILE_N,
-                    TILE_K=TILE_K,
-                    DYNAMIC_TILING=True,
-                )
+                for i in range(0, triton.cdiv(length, TILE_M)):
+                    _dispatch(
+                        pid_n,
+                        start_off + i * TILE_M, end_off,
+                        input, other + type_id * stride_other_b, output,
+                        K, N,
+                        stride_input_m, stride_input_k,
+                        stride_other_k, stride_other_n,
+                        stride_output_m, stride_output_n,
+                        out_dtype=out_dtype,
+                        MASK_M=True,
+                        EVEN_K=EVEN_K,
+                        EVEN_N=EVEN_N,
+                        TILE_M=TILE_M,
+                        TILE_N=TILE_N,
+                        TILE_K=TILE_K,
+                        DYNAMIC_TILING=True,
+                    )
             next_id = next_next_id
             next_next_id += 1
 
