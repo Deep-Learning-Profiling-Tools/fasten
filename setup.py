91d85868b65a288387bb5c90e11e61cc2c99a2e4
Jokeren <robinho364@gmail.com>
2024-04-16 09:06:41 -0400

    Update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='fasten',  # Required
-    version='0.0.1',  # Required
+    version='0.1',  # Required
     description='A libary for fast segment operators',  # Optional
 
     author='Keren Zhou',  # Optional
@@ -11,34 +11,14 @@ setup(
     author_email='kerenzhou@outlook.com',  # Optional
 
     classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: BSD-2 License'
+        'License :: OSI Approved :: BSD-3 License'
     ],
 
     packages=find_packages(),  # Required
 
     python_requires='>=3.6, <4',
-
-    install_requires=['pytest',
-                      'flake8',
-                      'autopep8',
-                      'isort',
-                      'pre-commit'],  # Optional
-
-    extras_require={  # Optional
-        'test': ['pytest'],
-    },
-
-    project_urls={  # Optional
-        'Source': 'https://github.com/Jokeren/fasten'
-    },
-
+    install_requires=['pytest', 'flake8', 'autopep8', 'isort', 'pre-commit', 'pytest', 'pyg-nightly', 'pyg-lib@https://github.com/pyg-team/pyg-lib.git'],
     include_package_data=True,
     long_description_content_type='text/markdown'
 )
