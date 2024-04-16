3ec30ccccf7cbcb04ea2843dee89ae2199db893f
Jokeren <robinho364@gmail.com>
2024-04-16 09:07:35 -0400

    Update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),  # Required
 
     python_requires='>=3.6, <4',
-    install_requires=['pytest', 'flake8', 'autopep8', 'isort', 'pre-commit', 'pytest', 'pyg-nightly', 'pyg-lib@https://github.com/pyg-team/pyg-lib.git'],
+    install_requires=['pytest', 'flake8', 'autopep8', 'isort', 'pre-commit', 'pytest', 'pyg-nightly', 'pyg-lib@git+https://github.com/pyg-team/pyg-lib.git'],
     include_package_data=True,
     long_description_content_type='text/markdown'
 )
